3b26d6404067cf8f4b54b580dd46946805af30be
chucoding <chucoding@gmail.com>
2022-03-20 18:53:08 +0900

    project init

---
+++ app.py
@@ -0,0 +1,11 @@
+# flask server test
+
+from flask import Flask
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+ 
+if __name__ == '__main__':
+    app.run()
