afa0f7246fe785d0adef322730ac4de77e009aca
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-04-17 02:01:46 +0900

    svg 구조 잡기 - 1

--- api/export_svg.py
+++ api/export_svg.py
@@ -8,10 +8,11 @@ logger = logMng().get_logger('app')
 def write_calendar():
     print('write calendar')
     return '''
-        <svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <rect width="100%" height="100%" fill="yellow" />
-            <circle cx="150" cy="100" r="80" fill="green" />
-            <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">외계공룡</text>
+        <svg version="1.1" baseProfile="full" width="360" height="460" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100%" height="100%" fill="#3a3a3a" />
+            <text x="10" y="60" font-size="45" fill="white">TIME 00:00</text>
+            <text x="10" y="90" font-size="15" fill="skyblue">YYYY/MM/DD</text>
+            <rect x="0" y="110" width="100%" height="2" fill="white" fill-opacity="0.5" stroke-opacity="0.8"/>
         </svg>
         '''
 
