82392d8ef7d4c125b8999aa48272d0d6cc315e61
chucoding <chucoding@gmail.com>
2022-06-06 19:59:47 +0900

    modify Procfile for heroku available port

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 worker: python main.py
-web: uvicorn main:app
+web: uvicorn main:app --host=0.0.0.0 --port=${PORT}
--- main.py
+++ main.py
@@ -16,4 +16,4 @@ def show_calendar():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True, debug=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, debug=True)
