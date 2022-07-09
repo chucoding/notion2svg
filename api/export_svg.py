d1c1be39a7aa400cda801406b5681ff4ba11ae53
chucoding <chucoding@gmail.com>
2022-07-09 23:50:45 +0900

    no-cache => max-age (github issue)

--- api/export_svg.py
+++ api/export_svg.py
@@ -3,4 +3,4 @@ from template.calendar import WhiteCalendar
 
 def write():
     cal1 = WhiteCalendar()
-    return Response(content=cal1.get_calendar(), headers={"Cache-Control": "no-cache, no-store, must-revalidate", "Pragma": "no-cache", "Expires":"0"}, media_type="image/svg+xml")
+    return Response(content=cal1.get_calendar(), headers={"Cache-Control": "max-age=0"}, media_type="image/svg+xml")
