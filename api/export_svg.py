83d0806beadbe456ebcc68af1eec1a89deb59860
chucoding <chucoding@gmail.com>
2022-07-09 23:39:36 +0900

    calendar detail no-cache in github

--- api/export_svg.py
+++ api/export_svg.py
@@ -3,4 +3,4 @@ from template.calendar import WhiteCalendar
 
 def write():
     cal1 = WhiteCalendar()
-    return Response(content=cal1.get_calendar(), media_type="image/svg+xml", headers={"Cache-Control":"no-cache"})
+    return Response(content=cal1.get_calendar(), headers={"Cache-Control": "no-cache, no-store, must-revalidate", "Pragma": "no-cache", "Expires":"0"}, media_type="image/svg+xml")
