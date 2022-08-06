fbdc26e56f5d2d58d9e2c9228b73270f46caf7c7
chucoding <chucoding@gmail.com>
2022-08-06 19:00:40 +0900

    bugfix : csp allow aws image in svg

--- api/export_svg.py
+++ api/export_svg.py
@@ -3,4 +3,4 @@ from template.calendar import WhiteCalendar
 
 def write():
     cal1 = WhiteCalendar()
-    return Response(content=cal1.get_calendar(), headers={"Cache-Control": "max-age=0", "Content-Security-Policy": "img-src data: *"}, media_type="image/svg+xml")
+    return Response(content=cal1.get_calendar(), headers={"Cache-Control": "max-age=0", "Content-Security-Policy": "object-src href: 'unsafe-eval'"}, media_type="image/svg+xml")
