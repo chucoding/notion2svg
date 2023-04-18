1d0effefa8cdb6acb8e0c67ece66d0cfc35e5205
chucoding <chucoding@gmail.com>
2023-04-18 22:35:44 +0900

    refactor : reduce unnecessary resource usage

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI, Response
 from template.calendar import NotionCalendar
 
 app = FastAPI()
- 
+cal = NotionCalendar()
+
 @app.get('/')
 def show_calendar():
-    cal1 = NotionCalendar()
-    return Response(content=cal1.get_calendar(), headers={"Cache-Control": "max-age=0", "Content-Security-Policy": "object-src href: 'unsafe-eval'"}, media_type="image/svg+xml")
+    return Response(content=cal.get_calendar(), headers={"Cache-Control": "max-age=0", "Content-Security-Policy": "object-src href: 'unsafe-eval'"}, media_type="image/svg+xml")
 
 if __name__ == '__main__':
     import uvicorn
