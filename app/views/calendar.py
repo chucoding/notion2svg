c3436c9a4cb7489edb55481794d4663f2741a8bc
chucoding <chucoding@gmail.com>
2023-05-07 13:59:38 +0900

    refactor : unified calendar design

--- app/views/calendar.py
+++ app/views/calendar.py
@@ -127,34 +127,3 @@ class NotionCalendar(Calendar):
             svg_weeks=svg_weeks,
             svg_days=svg_days
         )
-
-
-class BlackCalendar(Calendar):
-    def get_calendar(self):
-        x_idx = 20
-        str_weeks = ''
-        for w in Calendar.weeks:
-            str_weeks += "<text x='%d' y='145' font-size='15' fill='white'>%s</text>\n" % (
-                x_idx, w)
-            x_idx += 60
-        return '''
-            <!DOCTYPE svg PUBLIC
-            "-//W3C//DTD SVG 1.1//EN"
-            "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-            <svg version="1.1" width="360" height="460" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <rect width="100%" height="100%" fill="#3a3a3a" />
-                <text x="10" y="60" font-size="45" fill="white">{time}</text>
-                <text x="10" y="90" font-size="15" fill="skyblue">{today}</text>
-                <rect x="0" y="110" width="100%" height="2" fill="white" fill-opacity="0.5" stroke-opacity="0.8"/>
-                {str_weeks}
-                <rect x="0" y="170" width="100%" height="1" fill="white" fill-opacity="0.5" stroke-opacity="0.8"/>
-                <rect x="0" y="230" width="100%" height="1" fill="white" fill-opacity="0.5" stroke-opacity="0.8"/>
-                <rect x="0" y="290" width="100%" height="1" fill="white" fill-opacity="0.5" stroke-opacity="0.8"/>
-                <rect x="0" y="350" width="100%" height="1" fill="white" fill-opacity="0.5" stroke-opacity="0.8"/>
-                <rect x="0" y="410" width="100%" height="1" fill="white" fill-opacity="0.5" stroke-opacity="0.8"/>
-            </svg>
-            '''.format(
-            time=Calendar.time,
-            today=Calendar.today,
-            str_weeks=str_weeks
-        )
