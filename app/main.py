07f2645f2c9d4b53eab0693fd1183aed67aaa19e
chucoding <chucoding@gmail.com>
2023-09-16 14:16:19 +0900

    fix : requires __main__ to run local environment

--- app/main.py
+++ app/main.py
@@ -15,5 +15,5 @@ cal = NotionCalendar()
 def show_calendar():
     return Response(content=cal.get_calendar(), headers={"Cache-Control": "max-age=0"}, media_type="image/svg+xml")
 
-if __name__ == 'main':
+if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, debug=True)
